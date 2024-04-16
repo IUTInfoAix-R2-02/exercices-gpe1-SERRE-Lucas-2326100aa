55de9fd79291d7bc3b2c33fca55ccc8c808fb873
Cassslu <lucas.serre.1@etu.univ-amu.fr>
2024-04-16 11:22:53 +0200

    exo 3 et 4

--- src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -1,7 +1,18 @@
 package fr.amu.iut.exercice3;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import static javafx.geometry.Pos.CENTER;
 
 public class MaPremiereFenetreJavaFX extends Application {
 
@@ -11,7 +22,35 @@ public class MaPremiereFenetreJavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //vbox
+        VBox vbx1 = new VBox();
+        vbx1.setAlignment(CENTER);
+
+        //creation du label et ajout a la vbox
+        Label labelHello = new Label("Bonjour");
+        vbx1.getChildren().add(labelHello);
+
+        //ajout d'un champ
+        TextField fieldName = new TextField("veuillez saisir votre nom");
+        fieldName.setMaxWidth(180.0d);
+        fieldName.setFont(Font.font("Courier",FontWeight.NORMAL, 12));
+        vbx1.getChildren().add(fieldName);
+
+        //ajout d'un bouton
+        Button btnHello = new Button("dire bonjour");
+        vbx1.getChildren().add(btnHello);
+
+        // Changement du texte après un clic sur le bouton
+        btnHello.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            labelHello.setText( "Bonjour à toi, "+fieldName.getText() );
+        });
+
+        //details de la page
+        Scene scene = new Scene(vbx1);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("La page d'un Pro de JavaFX");
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(400);
         primaryStage.show();
     }
 }
--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,12 +1,18 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import static javafx.geometry.Pos.CENTER;
 
 public class Palette extends Application {
 
@@ -25,6 +31,49 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        BorderPane bdp = new BorderPane();
+
+        //zone bouton
+        HBox hbxBtn = new HBox();
+        hbxBtn.setAlignment(CENTER);
+        vert = new Button("Vert");
+        rouge = new Button ("Rouge");
+        bleu = new Button("Bleu");
+        hbxBtn.getChildren().addAll(vert, rouge, bleu);
+        bdp.setBottom(hbxBtn);
+
+        //pane
+        Pane pn = new Pane();
+        bdp.setCenter(pn);
+
+        //label affichage de la phrase d'entete + compte
+        HBox hbxLbl = new HBox();
+        bdp.setTop(hbxLbl);
+        hbxLbl.setAlignment(CENTER);
+        Label labelHello = new Label("Choisissez une couleur");
+        hbxLbl.getChildren().add(labelHello);
+
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, vertClicked -> {
+            nbVert++;
+            pn.setStyle("-fx-background-color: green");
+            labelHello.setText( "vert choisi "+nbVert+"fois");
+        });
+
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, rougeClicked -> {
+            nbRouge++;
+            pn.setStyle("-fx-background-color: red");
+            labelHello.setText( "vert choisi "+nbRouge+"fois");
+        });
+
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, bleuCliked -> {
+            nbBleu++;
+            pn.setStyle("-fx-background-color: blue");
+            labelHello.setText( "vert choisi "+nbBleu+"fois");
+        });
+
+        //details de la page
+        Scene sc = new Scene(bdp,400, 200);
+        primaryStage.setScene((sc));
         primaryStage.show();
     }
 }
